49f1a76da9ad75e3328d163dcc57812c1b25abf2
Daniel Palmer <daniel@0x0f.com>
2022-06-04 11:02:33 +0900

    iio: st-accel: Add support for Silan SC7A20 and SC7A30E
    This chips are not from ST but seem to be clones of the ST parts.
    The register and bit settings in the datasheets match those in the
    LIS2DH datasheet.

--- drivers/iio/accel/st_accel_i2c.c
+++ drivers/iio/accel/st_accel_i2c.c
@@ -116,8 +116,16 @@ static const struct of_device_id st_accel_of_match[] = {
 		.data = LSM303C_ACCEL_DEV_NAME,
 	},
 	{
+		.compatible = "silan,sc7660-accel",
+		.data = SC7660_ACCEL_DEV_NAME,
+	},
+	{
 		.compatible = "silan,sc7a20",
 		.data = SC7A20_ACCEL_DEV_NAME,
+	},
+	{
+		.compatible = "silan,sc7a30e-accel",
+		.data = SC7A30E_ACCEL_DEV_NAME,
 	},
 	{},
 };
@@ -156,7 +164,9 @@ static const struct i2c_device_id st_accel_id_table[] = {
 	{ LIS2HH12_ACCEL_DEV_NAME },
 	{ LIS302DL_ACCEL_DEV_NAME },
 	{ LSM303C_ACCEL_DEV_NAME },
+	{ SC7660_ACCEL_DEV_NAME },
 	{ SC7A20_ACCEL_DEV_NAME },
+	{ SC7A30E_ACCEL_DEV_NAME },
 	{},
 };
 MODULE_DEVICE_TABLE(i2c, st_accel_id_table);
