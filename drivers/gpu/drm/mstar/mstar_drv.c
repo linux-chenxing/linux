65189e6a304947462f88cc64471abf8d5506e935
Daniel Palmer <daniel@0x0f.com>
2022-12-14 19:40:33 +0900

    rebase fixes: drm

--- drivers/gpu/drm/mstar/mstar_drv.c
+++ drivers/gpu/drm/mstar/mstar_drv.c
@@ -11,6 +11,7 @@
 
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_drv.h>
+#include <drm/drm_fbdev_generic.h>
 #include <drm/drm_fb_dma_helper.h>
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_gem_dma_helper.h>
